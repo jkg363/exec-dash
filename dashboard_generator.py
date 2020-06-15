5b0e44a3193e544d894f324e39d6c1e1c1aae1a7
jkg363 <65503702+jkg363@users.noreply.github.com>
2020-06-14 23:15:24 -0400

    Data folder joined

--- dashboard_generator.py
+++ dashboard_generator.py
@@ -5,18 +5,23 @@ import datetime
 import numpy as np
 import pandas as pd
 
-# csv_filepath = "data\sales-201803.csv"
+# csv_filename = "data\sales-201803.csv"
 
-csv_filepaths = []
-while True:
-    csv_filepath = input("Please input desired csv file: ")
-    if csv_filepath == csv_filepaths.append(csv_filepath):
-        break
-    else:
-    print(Hey, are you sure)
+csv_filepath = os.path.join(os.path.dirname(__file__), "data")
+
+from os import listdir
+from os.path import isfile, join
+salescsvs = [c for c in listdir(csv_filepath) if isfile(join(csv_filepath, c))]
+
+
+# while True:
+#     csv_filepath = input("Please input desired csv file: ")
+#     if csv_filepath == csv_filepaths.append(csv_filepath):
+#         break
+#     else:
+#     print(Hey, are you sure)
+
 df = pd.read_csv(csv_filepath, delimiter = ',', skiprows = 0)
-
-
 
 sales = df.to_dict("records")
 total_sales = 0
@@ -34,6 +39,12 @@ def to_usd(my_price):
     return f"${my_price:,.2f}"
 total_sales = to_usd(total_sales)
 
+grouped = (df.groupby(["product"])
+    .agg({"sales price":sum})
+    .sort_values(["sales price"],ascending=False))
+    #.style.format('${0:,.2f}') #change to USD
+    #.reset_index())
+
 print("-----------------------")
 print("MONTH: March 2018") #make dynamic
 
@@ -45,17 +56,13 @@ print("TOTAL MONTHLY SALES: " + str(total_sales))
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
 
-grouped = (df.groupby(["product"])
-    .agg({"sales price":sum})
-    .sort_values(["sales price"],ascending=False))
-    #.style.format("${0:,.2f}") #change to USD
-    #.reset_index())
 print(str(grouped))
 
 # print("  1) Button-Down Shirt: $6,960.35")
 # print("  2) Super Soft Hoodie: $1,875.00")
 # print("  3) etc.")
 
+# SOURCE: https://stackoverflow.com/questions/9271464/what-does-the-file-variable-mean-do/9271479
 # SOURCE: https://stackoverflow.com/questions/27842613/pandas-groupby-sort-within-groups
 # SOURCE: https://stackoverflow.com/questions/36073984/pandas-sorting-observations-within-groupby-groups
 # SOURCE: https://stackoverflow.com/questions/51866908/difference-between-as-index-false-and-reset-index-in-pandas-groupby/51933722 
